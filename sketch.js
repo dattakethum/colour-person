a2923aa5991e937ccb20b6ebd3350a81e518eb4a
dattakethum <87993393+dattakethum@users.noreply.github.com>
2021-08-31 20:43:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+  background(51)
+  box=createSprite(200,200,30,30)
+  
+}
+
+function draw() 
+{
+  if(keyIsDown(RIGHT_ARROW)){
+    background("blue")
+  }
+ 
+  if(keyIsDown(LEFT_ARROW)){
+    background("red")
+  }
+
+
+  if(keyIsDown(UP_ARROW)){
+    background("orange")
+  }
+
+  if(keyIsDown(DOWN_ARROW)){
+    background("green")
+  }
+drawSprites()
+}
+
+
+
+
